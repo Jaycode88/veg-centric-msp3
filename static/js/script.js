6c9357f4b09a6af98a5cb0280f1652d21e30cd24
Joe Seabrook <joeseabrook0306@gmail.com>
2023-09-04 14:58:01 +0000

    Convert Navbar functionality to jquery

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // Nav bar dropdown for mobile
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdownMobile = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(dropdownMobile);
-  });
+$(document).ready(function() {
+    var dropdownMobile = $('.dropdown-trigger');
+    dropdownMobile.dropdown();
+});
