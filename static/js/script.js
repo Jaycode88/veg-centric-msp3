6148e9dbb5909bf090a19c5989ae64c267234b6d
Joe Seabrook <joeseabrook0306@gmail.com>
2023-09-04 14:48:32 +0000

    Add Navbar css styling and JS functionality

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Nav bar dropdown for mobile
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownMobile = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(dropdownMobile);
+  });
